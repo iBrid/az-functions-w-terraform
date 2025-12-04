b87a234ff55acb618ecb2411f4378be113332433
iBrid <col.buche02@gmail.com>
2025-12-03 21:15:00 -0600

    Add app service and function app resources to main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,21 @@ resource "azurerm_storage_container" "container" {
   storage_account_name  = var.storage_account_name
   container_access_type = "private"
 }
+
+resource "azurerm_app_service" "appservice" {
+    name                = var.app_service_plan_name
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    app_service_plan_id = var.app_service_plan_name.id
+    tags                = var.tags
+}
+
+resource "azurerm_function_app" "name" {
+  location =  var.location
+  name = var.function_app_name
+  resource_group_name = var.resource_group_name
+  storage_account_name = var.storage_account_name
+  app_service_plan_id = var.app_service_plan_name.id
+  storage_account_access_key = azurerm_storage_account.stg.primary_access_key
+  version = var.runtime_version
+}
